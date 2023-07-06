aeb9c30bf127a512361ed55795a83958af1c7589
Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>
2023-07-06 09:56:05 +0200

    [5] - True and False Koans implemented

--- koans/TrueAndFalseKoans.php
+++ koans/TrueAndFalseKoans.php
@@ -7,5 +7,105 @@ defined('__') or define('__', null);
 
 class TrueAndFalseKoans extends TestCase
 {
-    // TODO: Add tests for == and ===, != and !== (FALSE, null, 0, "")
+    // Rule: You can only use true or false for the solution
+
+    /**
+     * @testdox 3 and '3' are the same thing, but, the type? (Equal operator)
+     */
+    public function testStringAndIntAreEqual()
+    {
+        $int = 3;
+        $str = '3';
+
+        $this->assertEquals(__, $int==$str);
+    }
+
+    /**
+     * @testdox 3 and '3' are identical, or not? (Identical operator)
+     */
+    public function testStringAndIntAreNotIdentical()
+    {
+        $int = 3;
+        $str = '3';
+
+        $this->assertEquals(true, $int===$str);
+    }
+
+    /**
+     * @testdox It's the same with Not Equal Operator
+     */
+    public function testWeCanUseNotEqual()
+    {
+        $int = 3;
+        $str = '3';
+
+        $this->assertEquals(__, $int!=$str);
+    }
+
+    /**
+     * @testdox What happen if we use Not Identical Operator
+     */
+    public function testWeCanUseNotIdentical()
+    {
+        $int = 3;
+        $str = '3';
+
+        $this->assertEquals(__, $int!==$str);
+    }
+
+    /**
+     * @testdox There are more comparison operators
+     */
+    public function testMoreComparisonOperators()
+    {
+        $age = 22;
+        $weight = 70;
+        $wingspan = 70;
+
+        $this->assertEquals(__, $age > $weight);
+        $this->assertEquals(__, $age <= $wingspan);
+        $this->assertEquals(__, $wingspan >= $weight);
+    }
+
+    /**
+     * @testdox When you don't say anything are you saying the truth?
+     */
+    public function testWhatHappenWithEmptyStrings()
+    {
+        $this->assertEquals(__, '');
+    }
+
+    /**
+     * @testdox Is 0 false?
+     */
+    public function testWhatHappenWithCero()
+    {
+        $this->assertEquals(__, 0);
+        $this->assertEquals(__, '0');
+    }
+
+    /**
+     * @testdox Is 'false' false?
+     */
+    public function testWhatHappenWithStrings()
+    {
+        $this->assertEquals(__, 'false');
+    }
+
+    /**
+     * @testdox What happen with empty arrays?
+     */
+    public function testWhatHappenWithEmptyArrays()
+    {
+        $this->assertEquals(__, boolval(array()));
+    }
+
+    /**
+     * @testdox What happen with NULL?
+     */
+    public function testWhatHappenWithNull()
+    {
+        $this->assertEquals(__, null);
+    }
+
 }
