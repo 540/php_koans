6886421f9408a6e7dba9bf33edf00f4bb2fefc13
Ivan Sola <113472661+IvanSolaDelgado@users.noreply.github.com>
2023-07-11 12:40:39 +0200

    [9] - Exceptions Koans implemented
    * [9] - Exceptions Koans implemented
    
    * [9] - Custom exception extracted to a new file
    
    * [9] - Phpunit stopOnFailure set to True
    
    * [9] - Naming improved
    
    ---------
    
    Co-authored-by: Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>

---
+++ koans/ExceptionsKoans.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PhpKoans;
+
+use Exception;
+use LogicException;
+use PhpKoans\Exceptions\CustomGenericException;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+defined('__') or define('__', null);
+
+class ExceptionsKoans extends TestCase
+{
+    /**
+     * @testdox Exceptions can be thrown using the `throw` keyword
+     */
+    public function testThrowException()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage(__);
+
+        throw new Exception('Something went wrong');
+    }
+
+    /**
+     * @testdox Catching exceptions using try-catch blocks
+     */
+    public function testCatchException()
+    {
+        try {
+            throw new Exception('Something went wrong');
+        } catch (Exception $exception) {
+            $this->assertEquals(__, $exception->getMessage());
+        }
+    }
+
+    /**
+     * @testdox Catching multiple exceptions using separate catch blocks
+     */
+    public function testCatchMultipleExceptions()
+    {
+        try {
+            throw new RuntimeException('Runtime exception occurred');
+        } catch (RuntimeException $exception) {
+            $this->assertEquals(__, $exception->getMessage());
+        } catch (Exception $exception) {
+            $this->fail('Should not catch this exception');
+        }
+    }
+
+    /**
+     * @testdox Catching multiple exceptions using a single catch block
+     */
+    public function testCatchMultipleExceptionsSingleBlock()
+    {
+        try {
+            throw new RuntimeException('Runtime exception occurred');
+        } catch (RuntimeException|LogicException $exception) {
+            $this->assertEquals(__, $exception->getMessage());
+        } catch (Exception $exception) {
+            $this->fail('Should not catch this exception');
+        }
+    }
+
+    /**
+     * @testdox The finally block is executed regardless of whether an exception is thrown or caught
+     */
+    public function testFinallyBlock()
+    {
+        try {
+            throw new Exception('Something went wrong');
+        } catch (Exception $exception) {
+            $this->assertEquals(__, $exception->getMessage());
+        } finally {
+            $this->assertTrue(__);
+        }
+    }
+
+    /**
+     * @testdox Custom exception classes can be defined by extending the base Exception class
+     */
+    public function testCustomExceptionClass()
+    {
+        $this->expectException(CustomGenericException::class);
+        $this->expectExceptionMessage(__);
+
+        throw new CustomGenericException();
+    }
+}
