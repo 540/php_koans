14599b310fa958b4a047dcb3424c8eed73a7d9a3
Ivan Sola <soladelgadoivan@gmail.com>
2023-07-12 12:01:02 +0200

    [16] - Adjust test and comments in StringManipulationKoans

--- koans/StringManipulationKoans.php
+++ koans/StringManipulationKoans.php
@@ -8,9 +8,9 @@ defined('__') or define('__', null);
 class StringManipulationKoans extends TestCase
 {
     /**
-     * @testdox One can interpolate variables in a double-quoted string.
+     * @test You can interpolate variables in a double-quoted string
      */
-    public function testStringInterpolationInDoubleQuotedString()
+    public function useStringInterpolationInDoubleQuotedString()
     {
         $value = "one";
         $string = "The value is $value";
@@ -19,9 +19,9 @@ class StringManipulationKoans extends TestCase
     }
 
     /**
-     * @testdox One cannot interpolate variables in a single-quoted string.
+     * @test You cannot interpolate variables in a single-quoted string
      */
-    public function testStringInterpolationWillNotWorkInSingleQuotedString()
+    public function stringInterpolationWillNotWorkInSingleQuotedString()
     {
         $value = "one";
         $string = 'The value is $value';
@@ -30,9 +30,9 @@ class StringManipulationKoans extends TestCase
     }
 
     /**
-     * @testdox Another option for variable iterpolation in a double-quoted string is with curly brackets.
+     * @test Another option for variable interpolation in a double-quoted string is with curly brackets
      */
-    public function testStringInterpolationWithCurlyBrackets()
+    public function useStringInterpolationWithCurlyBrackets()
     {
         $value = "one";
         $string = "The value is {$value}";
@@ -41,9 +41,9 @@ class StringManipulationKoans extends TestCase
     }
 
     /**
-     * @testdox If one wants to interpolate associative array elements, one must use curly brackets.
+     * @test If one wants to interpolate associative array elements, one must use curly brackets
      */
-    public function testInterpolatingAssociativeArrayElementsMustBeInCurlyBrackets()
+    public function interpolatingAssociativeArrayElementsMustBeInCurlyBrackets()
     {
         $values = ["test" => "one", "foo" => "two"];
         $string = "The value is {$values["test"]}";
@@ -52,35 +52,37 @@ class StringManipulationKoans extends TestCase
     }
 
     /**
-     * @testdox Heredoc interpolates like a double-quoted string.
+     * @test Heredoc interpolates like a double-quoted string
      */
-    public function testHeredocInterpolatesLikeADoubleQuotedString()
+    public function heredocInterpolatesLikeADoubleQuotedString()
     {
         $value = "one";
         $string = <<<EOT
-The value is $value
-EOT;
+        The value is $value
+        EOT;
+
         // Hint: First and last line breaks of a Heredoc don't count
         $this->assertEquals(__, $string);
     }
 
     /**
-     * @testdox Nowdoc interpolates like a single-quoted string.
+     * @test Nowdoc interpolates like a single-quoted string
      */
-    public function testNowdocInterpolatesLikeASingleQuotedString()
+    public function nowdocInterpolatesLikeASingleQuotedString()
     {
         $value = "one";
         $string = <<<'EOT'
-The value is $value
-EOT;
+        The value is $value
+        EOT;
+
         // Hint: First and last line breaks of a Heredoc don't count
         $this->assertEquals(__, $string);
     }
 
     /**
-     * @testdox One can format a string using sprintf.
+     * @test You can format a string using sprintf
      */
-    public function testStringFormattingWithSprintf()
+    public function useStringFormattingWithSprintf()
     {
         $product = "banana";
 
@@ -90,9 +92,9 @@ EOT;
     }
 
     /**
-     * @testdox When formatting a string with sprintf, use different type specifiers for the different types of variables.
+     * @test When formatting a string with sprintf, use different type specifiers for the different types of variables
      */
-    public function testStringFormattingWithSprintfTypeSpecifiers()
+    public function stringFormattingWithSprintfWithTypeSpecifiers()
     {
         $product = "bananas";
         $quantity = 3;
@@ -105,9 +107,9 @@ EOT;
     }
 
     /**
-     * @testdox Complex string formatting can be done with sprintf.
+     * @test Complex string formatting can be done with sprintf
      */
-    public function testComplexStringFormattingWithSprintf()
+    public function useComplexStringFormattingWithSprintf()
     {
         $product = "bananas";
         $quantity = 3;
@@ -125,9 +127,9 @@ EOT;
     }
 
     /**
-     * @testdox One can extract a substring from another string.
+     * @test You can extract a substring from another string
      */
-    public function testExtractAStringFromAString()
+    public function extractAStringFromAString()
     {
         $string = "Bacon, lettuce and tomato";
 
@@ -135,9 +137,9 @@ EOT;
     }
 
     /**
-     * @testdox One can get a single character from a string using an array index.
+     * @test You can get a single character from a string using an array index
      */
-    public function testYouCanGetASingleCharacterFromAString()
+    public function getASingleCharacterFromAString()
     {
         $string = "Bacon, lettuce and tomato";
 
@@ -145,9 +147,9 @@ EOT;
     }
 
     /**
-     * @testdox Strings can be split.
+     * @test Strings can be split
      */
-    public function testStringsCanBeSplit()
+    public function splitTheStringIntoAnArray()
     {
         $string = "Sausage Egg Cheese";
         $words = explode(" ", $string);
@@ -156,9 +158,9 @@ EOT;
     }
 
     /**
-     * @testdox Strings can be joined.
+     * @test Strings can be joined
      */
-    public function testStringsCanBeJoined()
+    public function joinTheArrayInAString()
     {
         $words = ["Now", "is", "the", "time"];
         $string = implode(" ", $words);
@@ -167,9 +169,9 @@ EOT;
     }
 
     /**
-     * @testdox One can change the case of strings.
+     * @test You can change the case of strings
      */
-    public function testChangeCaseOfStrings()
+    public function changeTheCaseOfTheStrings()
     {
         $this->assertEquals(__, ucwords('one hand clap'));
         $this->assertEquals(__, strtoupper('one hand clap'));
